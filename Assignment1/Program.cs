b3b9a7b82ba6d88a47fa61e1a94b036bd6fd4a1d
Sneh Sanjivkumar Joshi <Sjoshi5071@conestogac.on.ca>
2019-09-30 12:08:34 -0400

    Change1

---
+++ Assignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program TriangleSolver = new Program();
+
+            Triangle triangle = TriangleSolver.DefaultRectangle();
+            int menuOption;
+
+            menuOption = TriangleSolver.checkselection();
+
+            switch(menuOption)
+            {
+
+            }
+            
+            
+        }
+
+        
+           
+            
+        
+    }
+}
---
+++ Assignment1/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Triangle
+    {
+        internal string GetLength()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool SetLength(int len)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
