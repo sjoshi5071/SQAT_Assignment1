0e49921474cde30edc59e45a4e57579496169454
Sneh Sanjivkumar Joshi <Sjoshi5071@conestogac.on.ca>
2019-09-30 12:36:57 -0400

    Change1

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -6,24 +6,11 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Program TriangleSolver = new Program();
-
-            Triangle triangle = TriangleSolver.DefaultRectangle();
-            int menuOption;
-
-            menuOption = TriangleSolver.checkselection();
-
-            switch(menuOption)
-            {
-
-            }
+           
             
             
         }
 
-        
-           
-            
-        
+
     }
 }
