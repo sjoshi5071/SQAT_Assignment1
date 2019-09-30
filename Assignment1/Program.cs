db5c78db58c6f7f042f06554053594312979e376
Sneh Sanjivkumar Joshi <Sjoshi5071@conestogac.on.ca>
2019-09-30 17:36:42 -0400

    second commit

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -4,13 +4,50 @@ namespace Assignment1
 {
     class Program
     {
+      
+
         static void Main(string[] args)
         {
-           
-            
-            
+
+            Console.WriteLine("Enter the dimensions as follows: ");
+            Console.ReadLine();
+            {
+                
+                string dim;
+                int idim;
+                int len1, len2, len3;
+                do
+                {
+                        //For length 1
+                        
+                        Console.WriteLine("Enter the dimesion for Length 1: ");
+                        int.TryParse(Console.ReadLine(), out len1);
+
+                        //For length 2
+                        
+                        Console.WriteLine("Enter the dimesion for Length 2: ");
+                        int.TryParse(Console.ReadLine(), out len2);
+
+                        //For length 3
+                        
+                        Console.WriteLine("Enter the dimesion for Length 3: ");
+                        int.TryParse(Console.ReadLine(), out len3);
+
+                    //calling function
+                    Console.WriteLine(TriangleSolver.TriangleType(len1, len2, len3));
+
+                    //If wants to continue or wants to exit
+                    Console.WriteLine("Enter 1 for entering the dimensions and 2 to exit.");
+                        dim = Console.ReadLine();
+                        int.TryParse(dim, out idim);
+                    
+                } while (idim == 1);
+
+                //calling function
+                //Console.WriteLine(TriangleSolver.TriangleType(len1, len2, len3));
+
+            }
+   
         }
-
-
     }
 }
---
+++ Assignment1/TriangleSolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public static class TriangleSolver
+    {
+        public static string TriangleType(int len1, int len2, int len3)
+            {
+            if ( len2 + len3 >=len1 || len1 + len2 >= len3 || len1 + len3 >= len2)
+            {
+
+                if (len1 == len2 && len2 == len3 && len3 == len1)
+
+                {
+                    return("Triangle is equilateral.");
+                }
+                else if (len1 == len2 || len2 == len3 || len3 == len1)
+                {
+                    return("Triangle is isosceles.");
+                }
+                else
+                {
+                    return ("Triangle is  is scalene.");
+                }
+            }
+            else
+            {
+                return ("Cannot form a triangle.");
+            }
+            
+
+            }
+
+    }
+}
